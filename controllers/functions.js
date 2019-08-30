8d70efd06be671f72d02902a478715acef020b8a
Mudassar Ali <mudassar.ali@jintech.com>
2019-08-30 12:20:55 +0500

    angular.forEach

--- controllers/functions.js
+++ controllers/functions.js
@@ -9,4 +9,14 @@ controller('copyExample',['$scope',function($scope){
 		angular.copy(user,$scope.leader);
 	};
 	$scope.reset();
+}]).
+controller('foreachExample',['$scope',function($scope){
+	$scope.fruits = [{name:'Apple',season:'winter'},{name:'Mango',season:'summer'}];
+	$scope.logs = [];
+	angular.forEach($scope.fruits,function(value,key){
+
+		this.push(value.name+" : "+value.season);
+		console.log(value.name,"  ",key);
+	},$scope.logs);
+
 }]);
